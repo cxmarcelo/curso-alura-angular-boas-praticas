7769fbbd8fb7542a643517c40dcc457e47bead75
cxmarcelo <marcelo.c.b22@gmail.com>
2022-06-01 01:46:36 -0300

    Modulo 02-03 / login e cadastro de usuarios.

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,11 +4,14 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NewUserComponent } from './new-user/new-user.component';
+import { MessageModule } from '../components/message/message.module';
 
 @NgModule({
-  declarations: [HomeComponent, LoginComponent],
-  imports: [CommonModule, HomeRoutingModule, FormsModule],
+  declarations: [HomeComponent, LoginComponent, NewUserComponent],
+  imports: [CommonModule, HomeRoutingModule, FormsModule, MessageModule,
+    ReactiveFormsModule,],
   exports: [HomeComponent],
 })
-export class HomeModule {}
+export class HomeModule { }
--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/authentication/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  username = '';
+  password = '';
+
+
+  constructor(
+    private authService: AuthenticationService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  login() {
+    this.authService.authentication(this.username, this.password).subscribe(() => {
+        this.router.navigate(['animals']);
+      }, error => {
+        alert('Invalid username or password');
+        console.log(error);
+      }
+    );
   }
 
 }
