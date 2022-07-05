2a8dffc3c96600cf6339aeb3e643f8c2a0ff7fcd
cxmarcelo <marcelo.c.b22@gmail.com>
2022-07-05 01:15:02 -0300

    Validação customizada.

--- src/app/home/new-user/new-user.component.ts
+++ src/app/home/new-user/new-user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { lowercaseValidator } from './lowercase.validator';
 import { NewUser } from './new-user';
 import { NewUserService } from './new-user.service';
 
@@ -21,7 +22,7 @@ export class NewUserComponent implements OnInit {
     this.newUserForm = this.formBuilder.group({
       email: ['', Validators.required, Validators.email],
       fullName: ['', Validators.required, Validators.minLength(4)],
-      userName: [''],
+      userName: ['', [lowercaseValidator]],
       password: [''],
     });
   }
