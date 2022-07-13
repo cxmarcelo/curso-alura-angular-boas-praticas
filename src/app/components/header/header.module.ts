9ff37df535ae1ce301e9a8def6622e49196e558f
cxmarcelo <marcelo.c.b22@gmail.com>
2022-07-12 23:36:11 -0300

    Menu component.

--- src/app/components/header/header.module.ts
+++ src/app/components/header/header.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
 import { RouterModule } from '@angular/router';
+import { MenuModule } from '../menu/menu.module';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [
+    HeaderComponent
+  ],
   imports: [
     CommonModule,
     RouterModule,
+    MenuModule,
   ],
-  exports: [HeaderComponent]
+  exports: [
+    HeaderComponent
+  ]
 })
 export class HeaderModule { }
